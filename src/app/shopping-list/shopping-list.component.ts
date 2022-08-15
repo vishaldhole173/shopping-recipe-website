7d4cd174b23aef0bfc26703f8d1170fbdbf4df64
vishal.dhole <vishal.dhole@sarvaha.com>
2022-08-15 18:39:46 +0530

    added shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { Ingredient } from 'src/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-
+export class ShoppingListComponent implements OnInit, OnChanges {
+  ingredients: Ingredient[] = [
+    new Ingredient('Potato', 10),
+    new Ingredient('Tomato', 20)
+  ];
   constructor() { }
+  
+  ngOnChanges() {
+  }
 
   ngOnInit(): void {
   }
